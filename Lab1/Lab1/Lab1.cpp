658abe86826e30c9f110f729c321ab4ff28a387a
Denis <den.girevoi_43@mail.ru>
2025-02-26 21:57:42 +0300

    Lab1

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 double round(double value, double relative_error) {
-    int significant_digits = ceil(-log10(relative_error / 100.0)); // Определение значащих цифр
+    int significant_digits = ceil(-log10(relative_error / 100.0)); 
     return round(value * pow(10, significant_digits)) / pow(10, significant_digits);
 }
 
@@ -14,29 +14,43 @@ int main() {
     setlocale(LC_ALL,"RUS");
     //Ex(в)
 
+    double number;
+    cout << "Введите приближенное число: ";
+    cin >> number;
 
+    // Предельная абсолютная погрешность
+    double absoluteError = pow(10, floor(log10(number) - 1)) / 2;
 
+    // Предельная относительная погрешность
+    double relativeError = absoluteError / number;
+
+    // Вывод результатов
+    cout << "Приближенное число: " << number << endl;
+    cout << "Предельная абсолютная погрешность: " << absoluteError << endl;
+    cout << "Предельная относительная погрешность: " << relativeError << endl;
+
+    return 0;
 
     //Ex1(б)
 
-    //double value, reletive_error;
+    double value, reletive_error;
 
-    //// Ввод данных
-    //cout << "Введите число: ";
-    //cin >> value;
-    //cout << "Введите относительную погрешность (%) : ";
-    //cin >> reletive_error;
+    //Ввод данных
+    cout << "Введите число: ";
+    cin >> value;
+    cout << "Введите относительную погрешность (%) : ";
+    cin >> reletive_error;
 
-    //// Округление числа
-    //double rounded_value = round(value, reletive_error);
+    //Округление числа
+    double rounded_value = round(value, reletive_error);
 
-    //// Вычисление абсолютной погрешности
-    //double absolute_error = abs(value - rounded_value);
+    //Вычисление абсолютной погрешности
+    double absolute_error = abs(value - rounded_value);
 
-    //// Вывод результатов
-    //cout << fixed << setprecision(6);
-    //cout << "Округленное число: " << rounded_value << endl;
-    //cout << "Абсолютная погрешность: " << absolute_error << endl;
+    //Вывод результатов
+    cout << fixed << setprecision(6);
+    cout << "Округленное число: " << rounded_value << endl;
+    cout << "Абсолютная погрешность: " << absolute_error << endl;
 
 
     //Ex1(a)
@@ -47,7 +61,6 @@ int main() {
     cin >> exact1;
     cout << "Введите округленное значение первого выражения: ";
     cin >> approx1;
-
     cout << "Введите точное значение второго выражения: ";
     cin >> exact2;
     cout << "Введите округленное значение второго выражения: ";
